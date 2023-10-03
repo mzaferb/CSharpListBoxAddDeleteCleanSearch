74ad685d73104862c15314f580972cbd76357489
mzaferb <bildik@yandex.com>
2023-10-03 11:25:31 +0300

    Add project files.

---
+++ ListBoxAddDltClnSrc/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ListBoxAddDltClnSrc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDlt = new System.Windows.Forms.Button();
+            this.btnCln = new System.Windows.Forms.Button();
+            this.btnSrc = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "Tomato",
+            "Pepper",
+            "Eggplant"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 304);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "";
+            this.textBox1.Location = new System.Drawing.Point(196, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Tag = "";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(196, 48);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(155, 37);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDlt
+            // 
+            this.btnDlt.Location = new System.Drawing.Point(196, 91);
+            this.btnDlt.Name = "btnDlt";
+            this.btnDlt.Size = new System.Drawing.Size(155, 37);
+            this.btnDlt.TabIndex = 3;
+            this.btnDlt.Text = "Delete";
+            this.btnDlt.UseVisualStyleBackColor = true;
+            this.btnDlt.Click += new System.EventHandler(this.btnDlt_Click);
+            // 
+            // btnCln
+            // 
+            this.btnCln.Location = new System.Drawing.Point(196, 134);
+            this.btnCln.Name = "btnCln";
+            this.btnCln.Size = new System.Drawing.Size(155, 37);
+            this.btnCln.TabIndex = 4;
+            this.btnCln.Text = "Clean";
+            this.btnCln.UseVisualStyleBackColor = true;
+            this.btnCln.Click += new System.EventHandler(this.btnCln_Click);
+            // 
+            // btnSrc
+            // 
+            this.btnSrc.Location = new System.Drawing.Point(196, 279);
+            this.btnSrc.Name = "btnSrc";
+            this.btnSrc.Size = new System.Drawing.Size(155, 37);
+            this.btnSrc.TabIndex = 5;
+            this.btnSrc.Text = "Search";
+            this.btnSrc.UseVisualStyleBackColor = true;
+            this.btnSrc.Click += new System.EventHandler(this.btnSrc_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AccessibleName = "";
+            this.textBox2.Location = new System.Drawing.Point(196, 243);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 30);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Tag = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 327);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnSrc);
+            this.Controls.Add(this.btnCln);
+            this.Controls.Add(this.btnDlt);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDlt;
+        private System.Windows.Forms.Button btnCln;
+        private System.Windows.Forms.Button btnSrc;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ListBoxAddDltClnSrc/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxAddDltClnSrc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int i = listBox1.Items.IndexOf(textBox1.Text);
+            if (i > -1)
+            {
+                MessageBox.Show("Already on the list!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Clear();
+                textBox1.Focus();
+                return;
+            }
+
+            if (textBox1.Text != "")
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+            else
+            {
+                MessageBox.Show("The line is empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Focus();
+            }
+        }
+
+        private void btnDlt_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Choose an item from the list!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Focus();
+            }
+        }
+
+        private void btnCln_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                listBox1.Items.Clear();
+            }
+        }
+
+        private void btnSrc_Click(object sender, EventArgs e)
+        {
+            int i = listBox1.Items.IndexOf(textBox2.Text);
+            if (i > -1)
+            {
+                listBox1.SelectedIndex = i;
+            }
+            else
+            {
+                MessageBox.Show("Not on the list!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
